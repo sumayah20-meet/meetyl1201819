588a89c88de156deda9fcf9cd8aa7fe18713c479
sumayah20-meet <sumayah20@meet.mit.edu>
2018-10-17 12:23:15 -0400

    my first commit

---
+++ lab1.py
@@ -0,0 +1,49 @@
+#print("sumayah")
+##for name in my_name:
+	#print(name)
+#for i in range (100):
+	#print("sumayah")
+
+#number1=5
+#print(number1)
+#number2=2.5
+#print(number2)
+#numbers=[3,2,1]
+#for i in numbers:
+
+
+		#print(i)
+		#print(i*2)
+
+import 
+turtle.pemsize(25)
+turtle.color("black")
+turtle.penup()
+turtle.goto(-110,-25)
+tuurtle.pendown()
+turtle.circle(100)
+
+turtle.pemsize(25)
+turtle.color("red")
+turtle.penup()
+turtle.goto(310,-25)
+tuurtle.pendown()
+turtle.circle(100)
+
+
+turtle.pemsize(25)
+turtle.color("yellow")
+turtle.penup()
+turtle.goto(-55,-75)
+tuurtle.pendown()
+turtle.circle(100)
+
+turtle.pemsize(25)
+turtle.color("green")
+turtle.penup()
+turtle.goto(55,-75)
+tuurtle.pendown()
+turtle.circle(100)
+
+turtle.done()
+
---
+++ lab2.py
@@ -0,0 +1,19 @@
+def fun(n):
+	a=n[0] 
+	b=n[-1]
+	list1=[a,b]
+	return list1
+
+
+
+list2=[2,3,4,5]
+fun(list2)
+n=fun(list2)
+print(n)
+
+def fun2(x):
+	for i in l:
+		if i < 5:
+			print(i)
+
+fun(list1) 
